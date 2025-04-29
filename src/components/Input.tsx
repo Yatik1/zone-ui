0492e372bbfece1f4e9f018a75512fdfcbc023f5
Yatik1 <yatiksrivastava1@gmail.com>
2025-04-29 18:10:45 +0530

    Refactor Input component to handle file uploads and improve response handling

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,25 @@
 
-import { ArrowUp, File, Globe, PersonStanding, Plus, X } from 'lucide-react'
+import { ArrowUp, Globe, PersonStanding, Plus } from 'lucide-react'
 import useMessage from '../hooks/useMessage'
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
+import FileSegment from './ui/FileSegment';
+
+
+// interface FileInputProps {
+//   file:File | null, 
+//   fileQuery:string,
+// }
 
 function Input() {
 
   const {setChats, chats, setMessage:newMessage} = useMessage() as any;
 
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState<any>("")
   const [selectedResponse, setSeletedResponse] = useState<string>("short")
   const [loading, setLoading] = useState<boolean>(false)
   const [disabled, setDisabled] = useState<boolean>(true)
-  const [file, setFile] = useState<File | null>(null)
+  const [file, setFile] = useState<File | undefined>(undefined)
 
   const fileInput = useRef<HTMLInputElement>(null)
 
@@ -25,13 +32,29 @@ function Input() {
     if(selectedFile && (selectedFile?.name.includes(".txt") || selectedFile?.name.includes(".docx"))){
       setFile(selectedFile)
     } else {
-      setFile(null)
+      setFile(undefined)
     }
   }
 
-  async function shortResponse(e?:React.FormEvent) {
-    e?.preventDefault()
-    if(message.length === 0) return;
+  // async function uploadFile(e?:React.FormEvent) {
+  //   e?.preventDefault()
+  //   if(!file || message.length === 0) return;
+  //   try{
+  //     setLoading(true)
+  //     newMessage(message)
+  //     const formData = new FormData()
+  //     formData.append("file", file)
+  //     formData.append("message", message)
+
+  //     // const res = await axios.post()
+  //     console.log({file, message})
+  //   } catch(error) {
+  //     console.log("[File upload error]", error)
+  //   }
+    
+  // }
+
+  async function shortResponse() {
     try {
       setLoading(true)
       newMessage(message)
@@ -45,9 +68,7 @@ function Input() {
     }
   }
 
-  async function detailedResponse(e?:React.FormEvent) {
-    e?.preventDefault()
-    if(message.length === 0) return;
+  async function detailedResponse() {
     try {
       setLoading(true)
       newMessage(message)
@@ -61,30 +82,59 @@ function Input() {
     }
   }
 
+  async function fileSubmit() {
+      try {
+        setLoading(true)
+        newMessage(file?.name,message)
+        const formData = new FormData()
+        formData.append('file',file as any);
+        formData.append('message', message);
+
+        const res = await axios.post('http://localhost:8000/upload_file', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        })
+        setChats([...chats, res.data])
+      } catch (error) {
+        console.log("File upload error", error)
+      } finally{
+        removeFile()
+        setMessage("")
+        setLoading(false)
+      }
+  }
+
+  function removeFile() {
+    setFile(undefined); 
+    if(fileInput.current) fileInput.current.value = "";
+  }
+
+  function handleResponses(e?:React.FormEvent) {
+    e?.preventDefault()
+
+    if(message.length === 0) return;
+
+    if(fileInput.current?.value) {
+      fileSubmit()
+      return;
+    }
+
+    selectedResponse === "short" ? shortResponse() : detailedResponse()
+
+  }
+
   useEffect(() => {
     if(message.length>0) setDisabled(false);
     else setDisabled(true);
   },[message])
 
   return (
-    <form
-      onSubmit={(selectedResponse === "short" ? shortResponse : detailedResponse)}
-      className="relative transition-all duration-200 ease-in-out"
-    >
-
-
+ 
     <div className=' flex border h-fit bg-white border-gray-300 rounded-xl px-4 py-4 mb-1 flex-1 items-center justify-center gap-2'>
         <div className='w-full h-full flex flex-col items-start justify-center gap-2'>
           {file && (
-            <div className='w-full flex items-start justify-start p-1'>
-              <div className='relative w-fit h-fit flex items-center justify-start bg-[#4A87ED] px-3 py-4 rounded-md text-md text-white font-semibold gap-2'>
-                <File />
-                {file.name}
-                <button className='absolute right-1 top-1 bg-white rounded-full group flex items-center justify-center text-black cursor-pointer' onClick={() => {setFile(null)}}>
-                  <X className='w-[14px] h-[14px]' />
-                </button>
-              </div>
-            </div>
+            <FileSegment fileName={file?.name} remove={removeFile} />
           )}
           {selectedResponse !== "detailed" ? (
             <input
@@ -114,28 +164,28 @@ function Input() {
               />
               <Plus className='w-5 h-5' />
             </div>
-          <div className={`flex items-center justify-center gap-1 ${selectedResponse === "short" ? "bg-orange-300 text-orange-900 border border-orange-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("short")}}>
+            <div className={`flex items-center justify-center gap-1 ${selectedResponse === "short" ? "bg-orange-300 text-orange-900 border border-orange-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("short")}}>
               <PersonStanding className="w-4 h-4" />
               Short friendly Response
-          </div>
+            </div>
 
-          <div className={`flex items-center justify-center gap-1 ${selectedResponse === "detailed" ? "bg-emerald-300 text-emerald-900 border border-emerald-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("detailed")}}>
+            <div className={`flex items-center justify-center gap-1 ${selectedResponse === "detailed" ? "bg-emerald-300 text-emerald-900 border border-emerald-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("detailed")}}>
               <Globe className="w-4 h-4" />
               Detailed Response
+            </div>
           </div>
-          </div>
-
         </div>
 
+
         <button 
-          className={`flex-shrink-0 w-8 h-8 rounded-md  text-white flex items-center justify-center ${disabled && "hidden"} ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-linear-to-br to-zinc-600 from-black cursor-pointer "}`}
-          onClick={() => {selectedResponse === "short" ? shortResponse() : detailedResponse()}}
-          disabled={loading}
-        >
-          <ArrowUp className='w-5 h-5 rotate-90'/>
-        </button> 
+        className={`flex-shrink-0 w-8 h-8 rounded-full  text-white flex items-center justify-center ${disabled && "hidden"} ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-linear-to-b from-black to-gray-500  cursor-pointer "}`}
+        onClick={handleResponses}
+        disabled={loading}
+      >
+        <ArrowUp className='w-5 h-5'/>
+      </button> 
+
     </div>  
-    </form>
   )
 }
 
