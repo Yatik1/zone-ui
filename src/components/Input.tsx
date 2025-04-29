29d4f78a7f4b3b4c20fc04616563e5a87affce44
Yatik1 <yatiksrivastava1@gmail.com>
2025-04-29 22:27:33 +0530

    Refactor Input component to enhance file handling and improve user interaction

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,10 @@
 
-import { ArrowUp, Globe, PersonStanding, Plus } from 'lucide-react'
+import { ArrowUp, File, Globe, Plus, User } from 'lucide-react'
 import useMessage from '../hooks/useMessage'
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import FileSegment from './ui/FileSegment';
-
+import {easeIn,motion} from "motion/react"
 
 // interface FileInputProps {
 //   file:File | null, 
@@ -13,12 +13,14 @@ import FileSegment from './ui/FileSegment';
 
 function Input() {
 
+  const [loading, setLoading] = useState<boolean>(false)
+  const [disabled, setDisabled] = useState<boolean>(true)
+  const [isFile, setIsFile] = useState<boolean>(false)
+
   const {setChats, chats, setMessage:newMessage} = useMessage() as any;
 
   const [message, setMessage] = useState<any>("")
   const [selectedResponse, setSeletedResponse] = useState<string>("short")
-  const [loading, setLoading] = useState<boolean>(false)
-  const [disabled, setDisabled] = useState<boolean>(true)
   const [file, setFile] = useState<File | undefined>(undefined)
 
   const fileInput = useRef<HTMLInputElement>(null)
@@ -29,30 +31,14 @@ function Input() {
 
   const handleFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0]
-    if(selectedFile && (selectedFile?.name.includes(".txt") || selectedFile?.name.includes(".docx"))){
+    if(selectedFile && !selectedFile?.name.includes(".pdf")){
       setFile(selectedFile)
+      setIsFile(true)
     } else {
       setFile(undefined)
+      setIsFile(false)
     }
   }
-
-  // async function uploadFile(e?:React.FormEvent) {
-  //   e?.preventDefault()
-  //   if(!file || message.length === 0) return;
-  //   try{
-  //     setLoading(true)
-  //     newMessage(message)
-  //     const formData = new FormData()
-  //     formData.append("file", file)
-  //     formData.append("message", message)
-
-  //     // const res = await axios.post()
-  //     console.log({file, message})
-  //   } catch(error) {
-  //     console.log("[File upload error]", error)
-  //   }
-    
-  // }
 
   async function shortResponse() {
     try {
@@ -107,6 +93,7 @@ function Input() {
 
   function removeFile() {
     setFile(undefined); 
+    setIsFile(false)
     if(fileInput.current) fileInput.current.value = "";
   }
 
@@ -131,30 +118,44 @@ function Input() {
 
   return (
  
-    <div className=' flex border h-fit bg-white border-gray-300 rounded-xl px-4 py-4 mb-1 flex-1 items-center justify-center gap-2'>
+    <div className='relative flex border h-fit bg-white border-gray-300 rounded-xl px-4 py-4 mb-1 flex-1 items-center justify-center gap-2'>
         <div className='w-full h-full flex flex-col items-start justify-center gap-2'>
+          
           {file && (
             <FileSegment fileName={file?.name} remove={removeFile} />
           )}
-          {selectedResponse !== "detailed" ? (
-            <input
-            type='text'
-            className=' w-full outline-none resize-none mb-1'
-            value={message || ""}
-            onChange={(e:React.ChangeEvent<HTMLInputElement>) => {setMessage(e.target.value)}}
-            placeholder='Write your message...'
-          />
-          ) : (
-            <textarea
-            className='w-full outline-none resize-none'
-            value={message || ""}
-            onChange={(e:React.ChangeEvent<HTMLTextAreaElement>) => {setMessage(e.target.value)}}
-            placeholder='Write your query...'
-            rows={2}
-          />
-          )}
 
-          <div className={`flex items-start justify-center gap-2`}>
+          {isFile ? (
+              <input 
+                type='text'
+                name='file_input'
+                className='w-full outline-none resize-none mb-1'
+                value={message || ""}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
+                placeholder='Ask anything from the document.'
+              />
+            ) : selectedResponse !== "detailed" ? (
+              <input
+                type='text'
+                name='friendly_responses'
+                className='w-full outline-none resize-none mb-1'
+                value={message || ""}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}
+                placeholder='Lets Chat'
+              />
+            ) : (
+              <textarea
+                className='w-full outline-none resize-none'
+                name='research'
+                value={message || ""}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}
+                placeholder={`Ask Anything\n\u2022 For the given question, AI will give you detailed information`}
+                rows={2}
+              />
+            )
+          }
+
+          <div className={`flex flex-1 w-full items-center justify-center gap-2`}>
             <div className='flex items-center justify-center p-1 rounded-full border border-gray-300 cursor-pointer' onClick={handleDivClick}>
              <input 
                 type='file'
@@ -164,27 +165,56 @@ function Input() {
               />
               <Plus className='w-5 h-5' />
             </div>
-            <div className={`flex items-center justify-center gap-1 ${selectedResponse === "short" ? "bg-orange-300 text-orange-900 border border-orange-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("short")}}>
-              <PersonStanding className="w-4 h-4" />
-              Short friendly Response
+
+            <div className='flex items-center justify-start w-full gap-2'>
+
+              {isFile ? (
+                <button className={`flex items-center justify-start gap-1 bg-blue-300 text-blue-900 border border-blue-600  text-sm px-3 py-1 rounded-full`}>
+                  <File className="w-4 h-4" />
+                  <p className='w-fit'>Document</p>
+                </button>
+              ) : (
+                <>
+                  <button className={`flex items-center justify-start gap-1 ${selectedResponse === "short" ? "bg-orange-300 text-orange-900 border border-orange-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("short")}}>
+                  <User className="w-4 h-4" />
+                  <p className='w-fit'>Friend</p>
+                </button>
+
+                <button className={`flex items-center justify-center gap-1 ${selectedResponse === "detailed" ? "bg-emerald-300 text-emerald-900 border border-emerald-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("detailed")}}>
+                  <Globe className="w-4 h-4" />
+                  Detailed Information
+                </button>
+                </>
+              )}
+
             </div>
 
-            <div className={`flex items-center justify-center gap-1 ${selectedResponse === "detailed" ? "bg-emerald-300 text-emerald-900 border border-emerald-600" : "bg-gray-200 text-gray-800"} text-sm px-3 py-1 rounded-full cursor-pointer`} onClick={() => {setSeletedResponse("detailed")}}>
-              <Globe className="w-4 h-4" />
-              Detailed Response
+            <div className="flex items-end justify-end">
+              <button 
+                className={`flex-shrink-0 rounded-full  text-white flex items-center justify-center p-1 ${disabled && "hidden"} ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-linear-to-b from-black to-gray-500  cursor-pointer "}`}
+                onClick={handleResponses}
+                disabled={loading}
+              >
+                <ArrowUp className='w-5 h-5'/>
+              </button> 
             </div>
           </div>
         </div>
 
-
-        <button 
-        className={`flex-shrink-0 w-8 h-8 rounded-full  text-white flex items-center justify-center ${disabled && "hidden"} ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-linear-to-b from-black to-gray-500  cursor-pointer "}`}
-        onClick={handleResponses}
-        disabled={loading}
-      >
-        <ArrowUp className='w-5 h-5'/>
-      </button> 
-
+        {loading && selectedResponse!=="short" && (
+          <motion.div 
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: easeIn }}
+            className="absolute top-[-3rem] left-0 w-fit bg-linear-to-br from-stone-400 via-stone-300 to-stone-200 py-1 px-[0.4rem] border border-stone-400 rounded-lg flex items-center justify-center gap-2"
+          >
+            <span className="relative flex size-3">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-stone-500 opacity-75"></span>
+                <span className="relative inline-flex size-3 rounded-full bg-linear-to-br from-stone-700 via-stone-600 to-stone-400"></span>
+            </span>
+           <p className="text-sm">Analysing</p>
+          </motion.div>
+        )}
     </div>  
   )
 }
