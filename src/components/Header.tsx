cd9c46da6749aba4e3467625f29dbec37a37a864
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-01 13:22:30 +0530

    Update Header component styling and content for improved visual consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@
 
 function Header() {
   return (
-    <div className="flex items-center justify-between border-b border-zinc-200 p-3">
-        <h2 className="text-xl font-semibold">ChatGenius</h2>
-        <p className="text-stone-500 text-sm">Powered by AI</p>
+    <div className="flex items-center justify-between border border-b border-gray-200 p-3 bg-white">
+        <h2 className="text-2xl font-semibold ml-3 bg-gradient-to-r from-stone-700 via-stone-600 to-stone-500 bg-clip-text text-transparent">
+            Zone
+        </h2>
+        {/* <p className="text-stone-500 text-sm">Powered by AI</p> */}
     </div>
   )
 }
