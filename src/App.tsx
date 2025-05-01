a9fea8f7fe7dd637e6206592cd62b8ce0a9cc739
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-01 13:22:14 +0530

    Remove background color from App component for cleaner layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ChatContainer from './components/ChatContainer';
 
 function App() {
   return (
-    <div className='min-h-screen bg-gray-50'>
+    <div className='min-h-screen'>
       <ChatContainer />
     </div>
   );
