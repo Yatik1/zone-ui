b4b88facf2607e1813e573f9cba15ed37c5e3e08
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-03 22:54:32 +0530

    Implement routing for SignIn page and enhance Header component for user authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import Signin from './auth/SignIn';
 import ChatContainer from './components/ChatContainer';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<AppLayout />} />
+      <Route path='sign-in' element={<Signin />} />
+    </Routes>
+  );
+}
+
+function AppLayout() {
   return (
     <div className='min-h-screen'>
       <ChatContainer />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,29 @@
-
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+
+  const navigate = useNavigate()
+
   return (
     <div className="flex items-center justify-between border border-b border-gray-200 p-3 bg-white">
-        <h2 className="text-2xl font-semibold ml-3 bg-gradient-to-r from-stone-700 via-stone-600 to-stone-500 bg-clip-text text-transparent">
+        <h2 className="flex items-center justify-center text-2xl font-semibold ml-3 bg-gradient-to-r from-stone-700 via-stone-600 to-stone-500 bg-clip-text text-transparent">
             Zone
         </h2>
-        {/* <p className="text-stone-500 text-sm">Powered by AI</p> */}
+        <header className='flex items-center justify-center'>
+
+          <SignedOut>
+          <div className='bg-linear-to-br from-stone-700 via-stone-600 to-stone-400 text-white text-sm px-3 py-2 rounded-full shadow-lg/15' onClick={() => navigate("/sign-in")}> 
+            Sign in
+          </div>
+          </SignedOut>
+
+          <div className='flex items-center justify-center shadow-lg/20 w-fit h-fit rounded-full'>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          </div>
+        </header>
     </div>
   )
 }
