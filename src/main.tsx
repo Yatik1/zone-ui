ee7ad6ff60635f4bb20e18a138a7e6ac861e1346
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-03 22:54:36 +0530

    Refactor main.tsx to integrate ClerkProvider and BrowserRouter for user authentication

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import MessagesProvider from './context/MessageContext.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MessagesProvider>
-    <App />
-    </MessagesProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/' >
+      <BrowserRouter>
+        <MessagesProvider>
+          <App />
+        </MessagesProvider>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
