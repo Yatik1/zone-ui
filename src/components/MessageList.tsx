e2b82ffda72bd917b94f24fb17f7d27d2036b1ba
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-12 20:42:02 +0530

    Remove commented console.log statement from MessageList component for cleaner code

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -5,8 +5,6 @@ import MessageItem from './MessageItem'
 function MessageList() {
     const {message, chats} = useMessage() as any
 
-
-    // console.log(chats)
     if(!message && !chats.length) {
         return (
             <div className='w-full h-full flex flex-col items-center justify-center'>
