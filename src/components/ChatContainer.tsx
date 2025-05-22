75f7f420b05e081cfc85d4bd6c43913a9a8d91d1
Yatik1 <yatiksrivastava1@gmail.com>
2025-05-22 13:06:05 +0530

    Refactor ChatContainer to enhance user creation logic and improve readability

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -5,19 +5,37 @@ import Input from './Input'
 import MessageList from './MessageList'
 import { useUser } from '@clerk/clerk-react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function ChatContainer() {
 
-  const {chats} = useMessage() as any;
-  const messagesEndRef = useRef<HTMLDivElement>(null);
+    const {chats} = useMessage() as any;
+    const messagesEndRef = useRef<HTMLDivElement>(null);
 
     const navigate = useNavigate()
-    const {isSignedIn} = useUser()
+    const {isSignedIn,user} = useUser()
+
+    const hasCreatedUser = useRef(false)
+
+    useEffect(() => {
+    if (isSignedIn && user?.id && user?.fullName && !hasCreatedUser.current) {
+      const createUser = async () => {
+        try {
+          await axios.post('http://localhost:8001/api/users/', {
+            user_id: user.id,
+            user_name: user.fullName
+          });
+          hasCreatedUser.current = true; 
+        } catch (error) {
+          console.log("Internal Error [USER CREATE ERROR]", error);
+        }
+      };
+      createUser();
+    }
+  }, [isSignedIn, user]);
 
   if(chats.length > 3 && !isSignedIn) {
-    (()=> {
       navigate("/auth/sign-in")
-    })()
   }
 
   useEffect(() => {
