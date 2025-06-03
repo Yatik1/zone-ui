35d368512dc62d60be6a94373fbff1b789d74d91
Yatik1 <yatiksrivastava1@gmail.com>
2025-06-03 23:59:41 +0530

    Refactor Header component to integrate useControl hook and improve layout structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,32 @@
 import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import { PanelLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
+import useControl from '../hooks/useControl';
 
 function Header() {
 
   const navigate = useNavigate()
+  const {isOpen, setIsOpen} = useControl() as any
 
   return (
-    <div className="flex items-center justify-between border border-b border-gray-200 p-3 bg-white">
-        <h2 className="flex items-center justify-center text-2xl font-semibold ml-3 bg-gradient-to-r from-stone-700 via-stone-600 to-stone-500 bg-clip-text text-transparent">
+    <div className="flex items-center justify-between border-b border-gray-200 p-3 bg-white">
+        
+         { !isOpen && (
+          <nav className='flex items-center justify-center'>        
+       
+          <small 
+            className='flex items-center justify-center text-gray-500 cursor-pointer hover:bg-gray-100 p-2 rounded-xl'
+            onClick={() => setIsOpen(true)}
+          >
+            <PanelLeft size={20}/>
+          </small>
+          <h2 className="flex items-center justify-center text-2xl font-semibold ml-3 bg-gradient-to-r from-stone-700 via-stone-600 to-stone-500 bg-clip-text text-transparent">
             Zone
         </h2>
-        <header className='flex items-center justify-center'>
+         </nav>
+         )
+         }
+        <header className='flex w-full items-center justify-end'>
 
           <SignedOut>
           <div className='bg-linear-to-br from-stone-700 via-stone-600 to-stone-400 text-white text-sm px-3 py-2 rounded-full shadow-lg/15' onClick={() => navigate("/auth/sign-in")}> 
