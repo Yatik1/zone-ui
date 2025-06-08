c4d093eef69b71b26253b74d458728e49dd14e69
Yatik1 <yatiksrivastava1@gmail.com>
2025-06-08 18:15:49 +0530

    Enhance MenuDrop component with hover effects for better user interaction

--- src/components/ui/MenuDrop.tsx
+++ src/components/ui/MenuDrop.tsx
@@ -2,18 +2,18 @@ import { Pen, Share, Trash } from "lucide-react"
 
 function MenuDrop({chatName, chatID} : {chatName:string, chatID?:string}) {
   return (
-    <div className="w-[7rem] absolute z-10 top-2 bg-white border border-gray-200 rounded-xl px-2 py-1.5 flex flex-col items-center justify-center">
-        <span className="flex items-center justify-center p-1 w-full gap-2" onClick={() => console.log({chatName,chatID})}>
+    <div className="w-[7rem] absolute z-10 top-[1rem] bg-white border border-gray-200 rounded-xl px-2 py-1.5 flex flex-col items-center justify-center">
+        <span className="flex items-center justify-center p-1 w-full gap-2 hover:bg-gray-50" onClick={() => console.log({chatName,chatID})}>
             <Pen size={15} /> 
             <p className="text-sm">Rename</p>
         </span>
 
-        <span className="flex items-center justify-center p-1 w-full gap-2">
+        <span className="flex items-center justify-center p-1 w-full gap-2 hover:bg-gray-50">
             <Share size={15} /> 
             <p className="text-sm">Share</p>
         </span>
 
-        <span className="flex items-center justify-center p-1 w-full gap-2 text-red-500">
+        <span className="flex items-center justify-center p-1 w-full gap-2 text-red-500 hover:bg-gray-50">
             <Trash size={15} /> 
             <p className="text-sm">Delete</p>
         </span>
