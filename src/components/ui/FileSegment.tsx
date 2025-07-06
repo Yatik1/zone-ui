90e62b52b508e5e67abe7f0e9733551ccf96d59b
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-06 14:29:17 +0530

    Add fileType prop to FileSegment for improved file type handling

--- src/components/ui/FileSegment.tsx
+++ src/components/ui/FileSegment.tsx
@@ -1,12 +1,15 @@
 import { File, FileText, X } from 'lucide-react'
 
-function FileSegment({fileName, remove, bg}:{fileName:string, remove?:() => void, bg?:string}) {
+function FileSegment({fileName, fileType ,remove}:{fileName:string, remove?:() => void, bg?:string, fileType?:string}) {
+    
+    
+
   return (
     <div>
-        <div className={`relative min-w-[13rem] w-fit h-fit flex items-center justify-start rounded-lg border border-stone-300 gap-2 p-1 ${bg ? `bg-${bg}` : "bg-gray-50" }`}>
-            <div className={`w-fit h-fit px-2 py-2 flex items-center justify-center bg-gradient-to-br ${fileName?.includes(".pdf") ?  "from-pink-600 via-pink-500 to-pink-400" : "from-blue-500 via-blue-400 to-blue-300"} rounded-md text-white`}>
+        <div className={`relative min-w-[13rem] w-fit h-fit flex items-center justify-start rounded-lg border border-stone-300 gap-2 p-1 bg-white }`}>
+            <div className={`w-fit h-fit px-2 py-2 flex items-center justify-center bg-gradient-to-br ${fileName?.includes(".pdf") || fileType==="pdf" ?  "from-pink-600 via-pink-500 to-pink-400" : "from-blue-500 via-blue-400 to-blue-300"} rounded-md text-white`}>
                 {
-                    fileName?.includes(".pdf") ? <File /> : <FileText />
+                    fileName?.includes(".pdf") || fileType === "pdf" ? <File /> : <FileText />
                 }
             </div>
             <p className='text-md'>{fileName}</p>
